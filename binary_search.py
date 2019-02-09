e3e29f580bfcf4fd01ce35c26ae18be44fbba6df
Nisvet Mujkic <nisvet.mujkic@infinitymesh.com>
2019-02-09 05:39:23 +0100

    binary search implementation and exercises

---
+++ binary_search.py
@@ -0,0 +1,36 @@
+def binary_search(elements, item):
+    low = 0
+    high = len(elements) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = elements[mid]
+
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
+
+elements = list(range(1, 101))
+
+print(binary_search(elements, 50))
+
+# EXERCISES
+
+# 1.1 Suppose you have a sorted list of 128 names, and you’re searching through it using binary search.
+# What’s the maximum number of steps it would take?
+
+# log128 = 7, because 2^7 = 128
+
+# 1.2 Suppose you double the size of the list.
+# What’s the maximum number of steps now?
+
+# log256 = 8, because 2^8 = 256
+
+
+
