68e314da100e6febfab6f20fe7a8bcfc72b8185a
Nisvet Mujkic <nisvet.mujkic@infinitymesh.com>
2019-02-20 05:27:56 +0100

    recursion exercise

--- recursion.py
+++ recursion.py
@@ -25,7 +25,7 @@ def print_recursive(num):
 def greet(name):
     print(f"Hi {name}")
     greet2(name)
-    print("Hehe")
+    print("END")
     bye()
 
 
@@ -54,3 +54,61 @@ def factorial(number):
 
 print(factorial(5))
 
+# Linear recursion = each recursion call is made only once
+
+arr = [1, 2, 3, 21, 334, 5, 6, 7, 8, -2]
+
+
+def sum_recursion(array, array_length):
+    if array_length == 0:
+        return 0
+    else:
+        return array[array_length - 1] + sum_recursion(array, array_length - 1)
+
+
+# print(sum_recursion(arr, len(arr)))
+
+
+def reverse_array(array, start, stop):
+    if start < stop - 1:
+        array[start], array[stop - 1] = array[stop - 1], array[start]
+        reverse_array(array, start + 1, stop - 1)
+
+
+# print(arr)
+
+# reverse_array(arr, 0, len(arr))
+
+# print(arr)
+
+# Write a short recursive Python function that finds the
+# minimum and maximum values in a sequence without using any loops.
+
+
+def find_min_max_recursive(array, length, min_value, max_value):
+    if length == 0:
+        return min_value, max_value
+    else:
+        if array[length - 1] < min_value:
+            min_value = array[length - 1]
+        elif array[length - 1] > max_value:
+            max_value = array[length - 1]
+        return find_min_max_recursive(array, length - 1, min_value, max_value)
+
+
+print(find_min_max_recursive(arr, len(arr), arr[0], arr[len(arr) - 1]))
+
+# Write a short recursive Python function that takes a character string s and outputs its reverse.
+# For example, the reverse of pots&pans would be snap&stop.
+
+s = "pots&pans"
+
+
+def reverse_string(s, length):
+    if length == 1:
+        return s[length - 1]
+    else:
+        return s[length - 1] + reverse_string(s, length - 1)
+
+
+print(reverse_string(s, len(s)))
